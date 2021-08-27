b7fd3a49fb5d0874fc1554227072afd6731977a5
hemahecodes <helena.martin@alum.esci.upf.edu>
2021-08-27 10:51:04 +0200

    PELE-530; Frag refactoring; fixed variable names and typo

--- frag_pele/Helpers/path.py
+++ frag_pele/Helpers/path.py
@@ -1,5 +1,5 @@
 
-class PathHandler:
+class PathHandler():
     def __init__(self,
                  PackagePath,
                  plop_path,
@@ -7,6 +7,7 @@ class PathHandler:
                  complex_pdb,
                  pdbout,
                  force_field,
+                 ID,
                  dist_constraint=None,
                  data=None,
                  documents=None,):
@@ -22,25 +23,26 @@ class PathHandler:
         pdbout
         force_field
         """
+        import os
+        self._current_path = current_path = os.path.abspath(".")
         self._plop_relative_path = os.path.join(PackagePath, plop_path)
         self._current_path = os.path.abspath(".")
         self._sch_python = os.path.join(sch_path, "utilities/python")
         self._pdb_basename = self._get_pdb_basename(complex_pdb)
-        self._working_dir = os.path.join(current_path, "{}_{}".format(pdb_basename, ID))
+        self._working_dir = os.path.join(current_path, "{}_{}".format(self._pdb_basename, ID))
+        if not os.path.exists(self._working_dir):
+            os.mkdir(self._working_dir)  # Creating a working directory for each PDB-fragment combination
 
-        if not os.path.exists(working_dir):
-            os.mkdir(working_dir)  # Creating a working directory for each PDB-fragment combination
-
-        self._pdbout_folder = os.path.join(working_dir, pdbout)
-        self._path_to_templates_generated, self._path_to_templates = self._get_path_to_templates_generated(working_dir, force_field)
-        self._path_to_lib = os.path.join(working_dir, "DataLocal/LigandRotamerLibs")
+        self._pdbout_folder = os.path.join(self._working_dir, pdbout)
+        self._path_to_templates_generated, self._path_to_templates = self._get_path_to_templates_generated(self._working_dir, force_field)
+        self._path_to_lib = os.path.join(self._working_dir, "DataLocal/LigandRotamerLibs")
         self._create_output_folder(self._working_dir)
-        self._create_symbolic_links(data,documents,working_dir)
+        self._create_symbolik_links(data,documents,self._working_dir)
         self._const = self._create_constraints(dist_constraint)
 
     def _create_output_folder(self, working_dir):
         from frag_pele.Helpers import folder_handler
-        folder_handler.check_and_create_DataLocal(working_dir=working_dir)
+        folder_handler.check_and_create_DataLocal(working_dir=self._working_dir)
 
     def _get_pdb_basename(self, complex_pdb):
         pdb_basename = complex_pdb.split(".pdb")[0]  # Get the name of the pdb without extension
@@ -49,7 +51,8 @@ class PathHandler:
         return pdb_basename
 
     def _get_path_to_templates_generated(self, working_dir, force_field):
-        from frag_pele.Erros.custom_errors import WrongForceField
+        from frag_pele.Errors.custom_errors import WrongForceField
+        import os
 
         if force_field == 'OFF':
             path_to_templates_generated = os.path.join(working_dir,
