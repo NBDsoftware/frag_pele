c558f26ef600843508eaa4aee9f56f263937b071
hemahecodes <helena.martin@alum.esci.upf.edu>
2021-08-27 15:47:53 +0200

    PELE-530; Frag REfactoring: Updated pdb handler.

--- frag_pele/Growing/pdbs.py
+++ frag_pele/Growing/pdbs.py
@@ -14,8 +14,81 @@ class PDBHandler():
             this fragment has been size-reduced in order to get small bond
             lengths between its atoms.
     """
-    def __init__(self):
+    def __init__(self, complex_pdb, pdb_fragment, pdb_atom_core_name, pdb_atom_fragment_name):
+        from rdkit import Chem
+        from rdkit.Chem import Draw
+        from rdkit.Chem.Draw import rdMolDraw2D
+        from rdkit.Chem import AllChem
+        from rdkit.Chem import rdchem
+
         """
 
         """
-        pass
+        # Check that ligand names are not repeated
+        # Get PDB components
+        pdb_ligand = self._get_pdb_components(complex_pdb)
+        # Generate RDKit molecules
+        fragment_mol, ligand_mol = self._create_rdkit_molecules(pdb_ligand, pdb_fragment)
+        # Join Molecules
+        self.grown_ligand = self._merge_ligand_and_fragment(fragment_mol, ligand_mol, pdb_atom_core_name, pdb_atom_fragment_name)
+
+
+    def _create_rdkit_molecules(self, pdb_ligand, pdb_fragment):
+        """
+
+        Parameters
+        ----------
+        complex_pdb : PDB file with protein-ligand complex.
+        pdb_fragment : PDB file with only the ligand.
+
+        Returns
+        -------
+        core_mol : RDKit molecule of initial seed compound.
+        framgent_mol : RDKit molecule of fragment.
+        """
+        fragment_mol = Chem.MolFromPDBFile(pdb_fragment)
+        ligand_mol = Chem.MolFromPDBFile(pdb_ligand)
+
+        return fragment_mol, ligand_mol
+
+    def _get_pdb_components(self, complex_pdb):
+        """
+        Split a protein-ligand pdb into protein and ligand components
+
+        """
+        import prody
+
+        pdb = prody.parsePDB(complex_pdb)
+        # protein = pdb.select('protein')
+        ligand = pdb.select('not protein and not water')
+        return ligand
+
+    def _merge_ligand_and_fragment(self, fragment_mol, ligand_mol, pdb_atom_core_name, pdb_atom_fragment_name):
+        """
+        Merge ligand and fragment an create bond between
+        user-specified atoms.
+        Parameters
+        ----------
+        fragment_mol
+        ligand_mol
+
+        Returns
+        -------
+
+        """
+        unlinked_molecules = Chem.CombineMols(fragment_mol, ligand_mol)
+        mw = Chem.RWMol(unlinked_molecules)
+
+        for atom in mw.GetAtoms():
+            if atom.GetPDBResidueInfo() == pdb_atom_core_name:
+                ligand_atom_idx = atom.GetIdx()
+            elif atom.GetPDBResidueInfo() == pdb_atom_fragment_name:
+                fragment_atom_idx = atom.GetIdx()
+
+        mw.ADDBond(ligand_atom_idx, fragment_atom_idx)
+        Chem.SanitizeMol(mw)
+        smiles = Chem.MolToSmiles(mw)
+        grown_ligand = Chem.MolFromSmiles(smiles)
+        return grown_ligand
+
+
